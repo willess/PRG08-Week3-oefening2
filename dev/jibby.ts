df9b3cd19f1bc51b769e5bab5a6ec59e84d52d14
Erik Katerborg <benni_jolink@hotmail.com>
2017-05-10 14:01:37 +0200

    Update jibby.ts

--- dev/jibby.ts
+++ dev/jibby.ts
@@ -17,44 +17,42 @@ class Jibby {
         this.y = 220;
         this.hygiene = this.food = this.happyness = 50;
 
-        // afbeelding voor idle - vervang dit door het gedrag
-        this.div.style.backgroundImage = "url('images/idle.png')";
-        // this.myBehavior = new Idle();
-
         // click listeners
         this.div.addEventListener("click", () => this.onPet());
         document.getElementsByTagName("foodbutton")[0].addEventListener("click", () => this.onEat());
         document.getElementsByTagName("washbutton")[0].addEventListener("click", () => this.onWash());
+
+        // hier het gedrag toekennen
+        // this.behavior = ...
         
+        // afbeelding voor idle - verplaatsen naar idle gedrag
+        this.div.style.backgroundImage = "url('images/idle.png')";
     }
 
     public update():void {
         // hier het gedrag updaten
-        //
+        // ...
         
-        // dit moet in het gedrag staan
+        // waarden verlagen per frame - dit moet in het gedrag staan
         this.hygiene -= 0.01;
         this.food -= 0.02;
         this.happyness -= 0.015;
-
-        // check of de waarden te laag zijn
-        // 
     }
 
 
     private onPet():void {
         console.log("you clicked on jibby!");
-        this.div.style.backgroundImage = "url('images/happy.png')";
+        // hier moet je de onPet functie van het gedrag aanroepen
     }
 
     private onWash():void {
         console.log("washing jibby!");
-        this.div.style.backgroundImage = "url('images/washing.png')";
+        // hier moet je de onWash functie van het gedrag aanroepen
     }
 
     private onEat():void {
         console.log("jibby is eating!");
-        this.div.style.backgroundImage = "url('images/eating.gif')";
+        // hier moet je de onEat functie van het gedrag aanroepen
     }
 
 
